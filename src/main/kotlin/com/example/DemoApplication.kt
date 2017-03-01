ec9f40caf2c7e2473df120392598e7d4a14097a7
David Komorowski <dkomo2@gmail.com>
2017-02-28 19:48:02 -0500

    Moves Firebase Configuration to its own class and file

--- src/main/kotlin/com/example/DemoApplication.kt
+++ src/main/kotlin/com/example/DemoApplication.kt
@@ -1,60 +1,10 @@
 package com.example
 
-import com.example.model.Person
-import com.example.validate.validate
-
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.DataSnapshot
-
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DemoApplication {
-    @Bean
-    fun init(fb: DatabaseReference) = CommandLineRunner {
-        /*
-         * Firebase Listeners to update the Person Repository
-         */
-        fb.addChildEventListener(object : ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot?, key : String?) {
-                val newPerson = dataSnapshot!!.getValue(Person::class.java)
-                validate(newPerson, fb, dataSnapshot.key)
-            }
-
-            override fun onChildChanged(dataSnapshot: DataSnapshot?, key: String?) {
-                val changedPerson = dataSnapshot!!.getValue(Person::class.java)
-                validate(changedPerson, fb, dataSnapshot.key)
-            }
-
-            override fun onChildRemoved(dataSnapshot: DataSnapshot?) {
-            }
-
-            override fun onChildMoved(dataSnapshot: DataSnapshot?, key: String?) {
-                val movedPerson = dataSnapshot!!.getValue(Person::class.java)
-                validate(movedPerson, fb, dataSnapshot.key)
-            }
-
-            override fun onCancelled(databaseError: DatabaseError?) {
-                if (databaseError != null) {
-                    throw databaseError.toException()
-                }
-            }
-        })
-        //These People will be added to the Repository by the Listeners
-        listOf(
-                Person("Jon", "Snow", "kotlin"),
-                Person("Daenerys", "Targaryen", "kotlin"),
-                Person("Tyrion", "Lannister", "kotlin"),
-                Person("Cersei", "Lannister", "javascript"),
-                Person("Sansa", "Stark", "java")
-        ).forEach{fb.child(it.id).setValue(it)}
-    }
-}
+open class DemoApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(DemoApplication::class.java, *args)
